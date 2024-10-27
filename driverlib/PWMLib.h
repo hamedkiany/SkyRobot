3e4a102e130d1ea940ddeb152d3a503ed571bd2d
hamedkiany <hamedkiany@gmail.com>
2024-10-27 13:49:06 +0100

    this is with Sharp

---
+++ driverlib/PWMLib.h
@@ -0,0 +1,23 @@
+/*
+ * PWMLib.h
+ *
+ *  Created on: 23 oct. 2024
+ *      Author: hamed
+ */
+
+#ifndef DRIVERLIB_PWMLIB_H_
+#define DRIVERLIB_PWMLIB_H_
+#include <stdint.h>
+
+extern int activatePWM(uint32_t VelocidadF2, uint32_t VelocidadF3);
+
+extern int configPWM1(uint32_t VelocidadF2);
+
+extern int configPWM2(uint32_t VelocidadF3);
+extern int forward();
+extern int rewind();
+extern int right();
+extern int left();
+
+
+#endif /* DRIVERLIB_PWMLIB_H_ */
---
+++ remotelink/remotelink_messages.h
@@ -0,0 +1,83 @@
+/*
+ * remotelink_messages.h
+ *
+ *  Created on: March. 2019
+ *
+ */
+
+#ifndef RL_MESSAGES_H
+#define RL_MESSAGES_H
+//Codigos de los mensajes y definicion de parametros para el protocolo RPC
+
+// El estudiante debe agregar aqui cada nuevo mensaje que implemente. IMPORTANTE el orden de los comandos
+// debe SER EL MISMO aqui, y en el codigo equivalente en la parte del microcontrolador (Code Composer)
+
+typedef enum {
+    MESSAGE_REJECTED,
+    MESSAGE_PING,
+    MESSAGE_LED_GPIO,
+    MESSAGE_LED_PWM_BRIGHTNESS,
+    MESSAGE_ADC_SAMPLE,
+    MESSAGE_ADC_Live,
+    MESSAGE_MOTOR,
+    //etc, etc...
+} messageTypes;
+
+//Estructuras relacionadas con los parametros de los mensajes. El estuadiante debera crear las
+// estructuras adecuadas a los mensajes usados, y asegurarse de su compatibilidad en ambos extremos
+
+#pragma pack(1) //Cambia el alineamiento de datos en memoria a 1 byte.
+
+typedef struct {
+    uint8_t command;
+} MESSAGE_REJECTED_PARAMETER;
+
+typedef union{
+    struct {
+         uint8_t padding:1;
+         uint8_t red:1;
+         uint8_t blue:1;
+         uint8_t green:1;
+    } leds;
+    uint8_t value;
+} MESSAGE_LED_GPIO_PARAMETER;
+
+typedef struct {
+    float rIntensity;
+} MESSAGE_LED_PWM_BRIGHTNESS_PARAMETER;
+
+typedef struct
+{
+    uint32_t chan1;
+    uint32_t chan2;
+    uint32_t chan3;
+    uint32_t chan4;
+    uint32_t chan5;
+    uint32_t chan6;
+    uint32_t chan7;
+    uint32_t chan8;
+
+} MESSAGE_ADC_SAMPLE_PARAMETER;
+typedef struct
+{
+    uint16_t chan1;
+    uint16_t chan2;
+    uint16_t chan3;
+    uint16_t chan4;
+    uint16_t chan5;
+    uint16_t chan6;
+    uint16_t chan7;
+    uint16_t chan8;
+
+} MESSAGE_ADC_LIVE_PARAMETER;
+typedef struct
+{
+    uint16_t x;
+    uint16_t y;
+} MESSAGE_MOTOR_PARAMETER;
+
+
+#pragma pack()  //...Pero solo para los comandos que voy a intercambiar, no para el resto.
+
+#endif // RPCCOMMANDS_H
+
